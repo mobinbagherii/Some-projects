3711132c75c83a30cfd7754552f19ec2aac98e15
Mobin <mobinbagheri4030@gmail.com>
2025-11-25 01:37:28 +0330

    Add files via upload

---
+++ crash.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+struct person {
+      string name ;
+      int age ;
+
+};
+int main () {
+      while (true ) {
+            person* p = new person;
+      }
+      cin.get() ;
+}
